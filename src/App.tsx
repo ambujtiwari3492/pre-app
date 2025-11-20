f7a62775c71bccb188d8adbef64e701f483a050c
ambujtiwari3492 <124238189+ambujtiwari3492@users.noreply.github.com>
2025-11-20 14:48:58 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='mb-4'>
+      Hello World
+     </h1>
+     <p>Ambuj Tiwari</p>
+    </>
+  )
+}
+
+export default App
